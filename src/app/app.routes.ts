5012855dc242331436ffd18b880c8034b2b540cf
Daniel Jose Ruiz Gutierrez <developer@danydev.co>
2024-10-19 16:58:30 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {AuthLayoutComponent} from "./auth/layouts/auth-layout/auth-layout.component";
+import {LoginComponent} from "./auth/pages/login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children:[
+            {
+                path: 'login',
+                component: LoginComponent,
+            }
+        ]
+    }
+];
