c6d4f8eac0d6fab634c0c233cc4d169c5d7410c9
Daniel Jose Ruiz Gutierrez <developer@danydev.co>
2024-10-19 21:52:39 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,43 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {AuthLayoutComponent} from "./auth/layouts/auth-layout/auth-layout.component";
 import {LoginComponent} from "./auth/pages/login/login.component";
+import {CreatePostComponent} from "./admin/components/create-post/create-post.component";
+import {RegisterComponent} from "./auth/pages/register/register.component";
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'auth',
         component: AuthLayoutComponent,
-        children:[
+        children: [
             {
                 path: 'login',
                 component: LoginComponent,
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
             }
         ]
+    },
+    {
+        path: 'admin',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'post',
+                component: LoginComponent,
+                children: [
+
+                ]
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            }
+        ]
+    },
+    {
+        path: 'admin',
+        component: CreatePostComponent
     }
 ];
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule
+    ],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    formGroup: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.formGroup = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.min(8)]]
+        });
+    }
+
+    send(){
+        console.log(this.formGroup.value)
+    }
+
 
 }
